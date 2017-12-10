71c8ca23e35968d187c324ced037fe6d9f7d0085
chanp <chanp@LAPTOP-FU3BI9IJ>
2017-12-09 20:40:27 -0500

    Used Event handler to display unique title.

--- Comp229-Assign03/Site2.Master.cs
+++ Comp229-Assign03/Site2.Master.cs
@@ -11,7 +11,21 @@ namespace Comp229_Assign03
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            switch (Page.Title)
+            {
+                case "Home Page":
+                    Page.Title = string.Format("RougeHill University");
+                    break;
+                case "Student":
+                    Page.Title = string.Format("Students Page");
+                    break;
+                case "Courses":
+                    Page.Title = string.Format("Courses Page");
+                    break;
+                case "Update":
+                    Page.Title = string.Format("Updates Page");
+                    break;
+            }
         }
     }
 }
